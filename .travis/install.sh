ada7c543afead59635c6cec74d0ef91d18602bec
David Davis <daviddavis@redhat.com>
2018-12-07 16:21:26 -0500

    Fixing pulpcore path
    [noissue]

--- .travis/install.sh
+++ .travis/install.sh
@@ -16,14 +16,13 @@ export PULP_FILE_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:
 export PULP_SMASH_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/PulpQE\/pulp-smash\/pull\/(\d+)' | awk -F'/' '{print $7}')
 
 if [ -z "$PULP_PR_NUMBER" ]; then
-  pip install "git+https://github.com/pulp/pulp.git#egg=pulpcore&subdirectory=pulpcore"
+  pip install "git+https://github.com/pulp/pulp.git#egg=pulpcore"
 else
   cd ../
   git clone https://github.com/pulp/pulp.git
   cd pulp
   git fetch origin +refs/pull/$PULP_PR_NUMBER/merge
   git checkout FETCH_HEAD
-  cd pulpcore
   pip install -e .
   cd ../pulpcore-plugin
 fi
