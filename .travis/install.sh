d6196a5f85d46357d55cec14a79596a033c01ff6
David Davis <daviddavis@redhat.com>
2019-02-15 12:11:14 -0500

    Test building docs against PRs
    Note that I am not creating a separate job for testing the docs. We're
    limited by the number of concurrent jobs in the Pulp org so I figure
    it's more efficient to execute the docs check during the normal test
    since it adds 30-60 seconds.
    
    fixes #4403
    https://pulp.plan.io/issues/4403

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,8 +2,8 @@
 set -v
 
 # dev_requirements should not be needed for testing; don't install them to make sure
-pip install "Django<=$DJANGO_MAX"
 pip install -r test_requirements.txt
+pip install -r doc_requirements.txt
 
 export COMMIT_MSG=$(git show HEAD^2 -s)
 export PULP_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulp\/pull\/(\d+)' | awk -F'/' '{print $7}')
