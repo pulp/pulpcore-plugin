8231e05692ea09fb37ef1ac7ded691cc42010c86
Austin Macdonald <austin@redhat.com>
2019-02-19 13:32:14 -0500

    Revert github.com/pulp/pulpcore-plugin/pull/58
    Merged prematurely, it depends RequiredPR:
    [pulp/pulp#3858](https://github.com/pulp/pulp/pull/3858)
    [noissue]

--- pulpcore/plugin/serializers.py
+++ pulpcore/plugin/serializers.py
@@ -16,6 +16,4 @@ from pulpcore.app.serializers import (  # noqa
     RelatedField,
     RepositorySyncURLSerializer,
     RepositoryPublishURLSerializer,
-    SingleContentArtifactField,
-    relative_path_validator,
 )
